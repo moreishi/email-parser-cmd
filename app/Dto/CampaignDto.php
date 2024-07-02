571a83adb288a9216d28fd09b6ef62a363035853
Caesar Ian Belza <moreishi@gmail.com>
2024-07-02 18:01:42 +0800

    add campaign

---
+++ app/Dto/CampaignDto.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Dto;
+
+class CampaignDto {
+
+    public $affilaite_id;
+    public $envelope;
+    public $from;
+    public $subject;
+    public $dkim;
+    public $SPF;
+    public $spam_score;
+    public $email;
+    public $raw_text;
+    public $sender_ip;
+    public $to;
+
+}
---
+++ app/Services/CampaignService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\ICampaignService;
+use App\Dto\CampaignDto;
+use App\Http\Resources\CampaignResource;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Models\Campaign;
+
+class CampaignService implements ICampaignService {
+
+    public function create(CampaignDto $dto) : CampaignResource {
+        $campaign = Campaign::create([
+            'email' => $dto->email,
+            'raw_text' => $dto->raw_text
+        ]);
+        return new CampaignResource($campaign);
+    }
+
+    public function update(CampaignDto $dto, int $id) : CampaignResource {
+        $campaign = Campaign::find($id);
+        $campaign->raw_text = $dto->raw_text;
+        $campaign->save();
+        return new CampaignResource($campaign);
+    }
+
+    public function delete(int $id) : JsonResponse {
+
+        $campaign = Campaign::findOrFail($id);
+        
+        if($campaign) {
+            $campaign->delete();
+            return new JsonResponse(['message' => 'Deleted'], 201);
+        }
+
+        return new JsonResponse(['message' => 'Bad request'], 400);
+    }
+
+}
